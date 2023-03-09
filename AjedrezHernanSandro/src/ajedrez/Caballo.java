eeba61bb5be08029d28ded69947bdc09269d942a
AL12031\USUARIO CCC <alesandroblanco24@gmail.com>
2023-03-09 13:49:21 +0100

    clase caballo

--- AjedrezHernanSandro/src/ajedrez/Caballo.java
+++ AjedrezHernanSandro/src/ajedrez/Caballo.java
@@ -1,5 +1,55 @@
 package ajedrez;
 
-public class Caballo {
+import java.util.Scanner;
 
+public class Caballo implements Piezas {
+	private String[] posiOriginal;
+
+	public Caballo(String[] posiOriginal, String color) {
+		this.posiOriginal = posiOriginal;
+
+	}
+
+	@Override
+	public String mover(boolean turnoBlancas) {
+		int filaEelegida = Integer.parseInt(posiOriginal[1]);
+		int columnaElegida = Integer.parseInt(posiOriginal[2]);
+		Scanner src = new Scanner(System.in);
+		while (true) {
+			System.out.println("Ingresa fila a donde mover ");
+			int filaMoverCaballo = src.nextInt();
+			System.out.println("Ingresa columna a donde mover ");
+			int columnaMoverCaballo = src.nextInt();
+			if (filaMoverCaballo == filaEelegida - 2 && columnaMoverCaballo == columnaElegida + 1
+					|| filaMoverCaballo == filaEelegida - 1 && columnaMoverCaballo == columnaElegida + 2
+					|| filaMoverCaballo == filaEelegida + 1 && columnaMoverCaballo == columnaElegida + 2
+					|| filaMoverCaballo == filaEelegida + 2 && columnaMoverCaballo == columnaElegida + 1
+					|| filaMoverCaballo == filaEelegida + 2 && columnaMoverCaballo == columnaElegida - 1
+					|| filaMoverCaballo == filaEelegida + 1 && columnaMoverCaballo == columnaElegida - 2
+					|| filaMoverCaballo == filaEelegida - 1 && columnaMoverCaballo == columnaElegida - 2
+					|| filaMoverCaballo == filaEelegida - 2 && columnaMoverCaballo == columnaElegida - 1) {
+				// Verificamos que la posición a la que se quiere mover esté vacía o tenga una
+				// pieza del
+				// color opuesto
+				if (Tablero.tablero[filaMoverCaballo][columnaMoverCaballo].equals(" *  ")
+						|| (turnoBlancas && Tablero.tablero[filaMoverCaballo][columnaMoverCaballo].contains("n"))
+						|| (!turnoBlancas && Tablero.tablero[filaMoverCaballo][columnaMoverCaballo].contains("b"))) {
+					Tablero.tablero[filaMoverCaballo][columnaMoverCaballo] = "C" + (turnoBlancas ? "b" : "n");
+					Tablero.tablero[filaEelegida][columnaElegida] = " *  ";
+					posiOriginal[1] = Integer.toString(filaMoverCaballo);
+					posiOriginal[2] = Integer.toString(columnaMoverCaballo);
+					return Tablero.tablero[filaMoverCaballo][columnaMoverCaballo];
+				} else {
+					System.out.println("No puedes mover a esa posición");
+				}
+			} else {
+				System.out.println("Movimiento no válido");
+			}
+		}
+	}
+
+	@Override
+	public double comer() {
+		return 0;
+	}
 }
--- AjedrezHernanSandro/src/ajedrez/Peon.java
+++ AjedrezHernanSandro/src/ajedrez/Peon.java
@@ -32,15 +32,7 @@ public class Peon extends Juego implements Piezas {
 
 	@Override
 	public double comer() {
-/*		   String[] posicionIngresada =Juego.PosiIngresada();
-		   String [] posicionAMover =Juego.PosiMover();
-		   
-		    if (posicionAMover[0].contains("n")) {
-		        // Reemplazamos la posición actual con un asterisco
-		        Tablero.tablero[Integer.parseInt(posicionIngresada[1])][Integer.parseInt(posicionIngresada[2])] = " *  ";
-		        // Movemos la pieza a la posición deseada
-		        Tablero.tablero[Integer.parseInt(posicionAMover[1])][Integer.parseInt(posicionAMover[2])] = posicionIngresada[0];
-		    }*/
+
 		return 0;
 	}
 
